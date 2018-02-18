3d65961cc2945ab662a3453106917abac4664e26
Cesar Denis <cesar@cesardenis.com>
2018-02-17 22:32:16 -0300

    feature: add browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+const localServer = {
+  path: 'localhost/',
+  port: 9000
+};
+
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -10,5 +16,19 @@ module.exports = {
     path: path.resolve(__dirname, '.tmp'),
     publicPath: '/'
   },
-  plugins: [new CleanWebpackPlugin(['.tmp'])]
+  plugins: [
+    new CleanWebpackPlugin(['.tmp']),
+    new BrowserSyncPlugin({
+      host: localServer.path,
+      port: localServer.port,
+      server: { baseDir: ['.tmp', 'src'] },
+      files: ['**/*.html'],
+      injectChanges: true,
+      logFileChanges: true,
+      logLevel: 'debug',
+      logPrefix: 'wepback',
+      notify: true,
+      reloadDelay: 0
+    })
+  ]
 };
